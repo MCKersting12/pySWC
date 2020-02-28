6e6e1b4531b5753b748d913c7aa8ba39a8b114fe
Matthew Kersting <52083309+MCKersting12@users.noreply.github.com>
2020-02-28 16:10:24 -0500

    got lists of swellings

--- record_swellings.py
+++ record_swellings.py
@@ -15,7 +15,7 @@ def main(input_swc):
     
     swc = pySWC.Swc(input_swc)
     number, parent = swc.node_number, swc.node_parent
-    label, radius = swc.node_number, swc.radius
+    label, radius = swc.node_label, swc.node_radius
     num_nodes = swc.num_nodes
     swelling_label = 18
     
@@ -24,16 +24,49 @@ def main(input_swc):
     
     for node in range(0, num_nodes):
         
-        swelling = [node]
+        swelling = []
+        if swc.data[node, label] == swelling_label:
+            break_var = False
+        else:
+            break_var = True
         
-        if node not in checked_nodes and swc.data[node, label] == swelling_label:
-            child = swc.find_children(node)
-            if swc.data[node, label] === swelling_label:
-                swelling.append(child)
+        while node not in checked_nodes and break_var == False:
             
-    
-    
+            swelling.append(node)
+            children = swc.find_children(node)
+            checked_nodes.add(node)
+            if children:
+                child = -1
+                if len(children) > 1:
+                    for kido in children:
+                        if swc.data[kido, label] == swelling_label:
+                            child = kido    
+                else:
+                    child = children[0]
+                
+                if swc.data[child, label] == swelling_label:
+                    node = child
+                
+                else:
+                    break_var = True
+            
+            else:
+                break_var = True
+        
+        if swelling:
+            print(swelling)
+        
+        largest = 0.0
+        for swelling_node in swelling:
+            if swc.data[swelling_node, radius] > largest:
+                largest = swc.data[swelling_node, radius]
+        #print(largest)
+                
+            
 
 if __name__ == "__main__":
-
-    main(sys.argv[1])
+    
+    #if len(sys.argv) > 1:
+    main('VCN_c09_Full.swc')
+    #else:
+    #    print('Usage: python record_swelings.py [swc file path]')
