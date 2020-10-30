0b89784b575105467bc7c80bd397077680126d97
Matthew Kersting <52083309+MCKersting12@users.noreply.github.com>
2020-10-30 13:10:43 -0400

    more tests

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 import unittest
 import pyswc
 
-test_file = pyswc.Swc('test_file.swc')
-
 
 class TestSwc(unittest.TestCase):
 
     def test_isolate_type(self):
+
+        test_file = pyswc.Swc('test_file.swc')
 
         isolated = pyswc.isolate_type(test_file, [2])
 
@@ -14,11 +14,21 @@ class TestSwc(unittest.TestCase):
 
     def test_surface_area_calculation(self):
 
+        test_file = pyswc.Swc('test_file.swc')
+
         isolated = pyswc.isolate_type(test_file, [4])
 
         target = 114.06
 
         self.assertEqual(round(isolated.surface_area, 2), target)
+
+    def test_scale_function(self):
+
+        test_file = pyswc.Swc('test_file.swc')
+
+        test_file.scale(0.1)
+
+        self.assertEqual(round(test_file.data[0][2], 2), -15.39)
 
 
 if __name__ == "__main__":
